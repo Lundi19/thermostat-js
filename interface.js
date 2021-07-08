c1cd59e142821c61dec3df79a0498d90db2f7696
LUNDI19 <michaelmcloughlin9@gmail.com>
2021-07-08 17:35:09 +0200

    added weather api

--- interface.js
+++ interface.js
@@ -39,4 +39,36 @@ document.addEventListener("DOMContentLoaded", () => {
     document.querySelector('#power-save-status').innerText = 'off';
     updateTemperature();
   })
+
+  // fetch('http://api.openweathermap.org/data/2.5/weather?q=London,uk&appid=a3d9eb01d4de82b9b8d0849ef604dbed&units=metric')
+  // .then((response) => {
+  //   return response.json()
+  // })
+  // .then((data) => {
+  //   document.querySelector('#current-temperature').innerText = data.main.temp;
+  // });
+  const displayWeather = (city) => {
+    const url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=a3d9eb01d4de82b9b8d0849ef604dbed&units=metric`
+  
+    fetch(url)
+      .then((response) => {
+        return response.json()
+      })
+      .then((data) => {
+        document.querySelector('#current-temperature').innerText = data.main.temp;
+      })
+  }
+  
+  displayWeather('London');
+
+  document.querySelector('#select-city').addEventListener('submit', (event) => {
+    // event.preventDefault();
+    const city = document.querySelector('#current-city').value;
+
+    displayWeather(city);
+})
+
 });
+
+
+
